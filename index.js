e4cfbc5d83290396b3d08b78f06694221b00df6e
akinpiers <fatunmbip@gmail.com>
2025-02-26 11:58:20 +0100

    First Commit

---
+++ index.js
@@ -0,0 +1,39 @@
+function addition(num1,num2) {
+//Addition operation
+    let result = num1 + num2
+
+    return result
+
+}
+
+console.log(addition(1000,1000))
+
+//Substraction
+
+function Substraction(num1,num2){
+
+    let result = num1 - num2
+    return result
+}
+
+console.log(Substraction(10000,500))
+
+//Division
+
+function division(num1,num2){
+
+    let result = num1/num2
+    return result
+}
+
+console.log(division(10000,50))
+
+//Modulus
+
+function modulus(num1,num2){
+
+    let result = num1%num2
+    return result
+}
+
+console.log(modulus(100,40))
